a337c266ad2e1030c42b26eb6efc67537605915e
Ivan Lytovka <50273967+lytovka@users.noreply.github.com>
2023-01-01 08:37:28 -0500

    chore: update release-notes-generator config (#10)

--- .releaserc.js
+++ .releaserc.js
@@ -32,7 +32,71 @@ module.exports = {
         },
       },
     ],
-    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "conventionalcommits",
+        presetConfig: {
+          types: [
+            {
+              type: "refactor",
+              section: "Build",
+              hidden: false,
+            },
+            {
+              type: "docs",
+              section: "Docs",
+              hidden: false,
+            },
+            {
+              type: "refactor",
+              section: "Refactor",
+              hidden: false,
+            },
+            {
+              type: "ci",
+              section: "CI",
+              hidden: false,
+            },
+            {
+              type: "style",
+              section: "Style",
+              hidden: false,
+            },
+            {
+              type: "test",
+              section: "Tests",
+              hidden: false,
+            },
+            {
+              type: "chore",
+              section: "Chores",
+              hidden: false,
+            },
+            {
+              type: "perf",
+              section: "Performance",
+              hidden: false,
+            },
+            {
+              type: "fix",
+              section: "Fixes",
+              hidden: false,
+            },
+            {
+              type: "feat",
+              section: "Features",
+              hidden: false,
+            },
+            {
+              type: "revert",
+              section: "Revert",
+              hidden: false,
+            },
+          ],
+        },
+      },
+    ],
     "@semantic-release/changelog",
     "@semantic-release/npm",
     "@semantic-release/git",
