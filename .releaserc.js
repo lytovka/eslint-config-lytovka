a3176037e86bebb2d251cb0527e3ca311cf46b8f
ivanlytovka <ivanlytovka@gmail.com>
2023-01-01 08:03:09 -0500

    chore: modify releaseRules to specify version stragery for each commit type

--- .releaserc.js
+++ .releaserc.js
@@ -8,6 +8,30 @@ module.exports = {
     { name: "alpha", prerelease: true },
   ],
   plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "angular",
+        releaseRules: [
+          { breaking: true, release: "major" },
+          { type: "feat", release: "minor" },
+          { type: "fix", release: "patch" },
+          { type: "build", release: "patch" },
+          { type: "chore", release: "patch" },
+          { type: "ci", release: "patch" },
+          { type: "docs", release: "patch" },
+          { type: "perf", release: "patch" },
+          { type: "refactor", release: "patch" },
+          { type: "revert", release: "patch" },
+          { type: "style", release: "patch" },
+          { type: "test", release: "patch" },
+          { scope: "no-release", release: false },
+        ],
+        parserOpts: {
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"],
+        },
+      },
+    ],
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     "@semantic-release/npm",
