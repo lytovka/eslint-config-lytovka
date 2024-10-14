d7d011fc3b2f223488dbca621e150d9eaaae1171
ivanlytovka <ivanlytovka@gmail.com>
2024-10-14 15:04:17 -0400

    add jsx and react flat configs

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,2 +1,4 @@
 export * from "./javascript"
 export * from "./typescript"
+export * from "./jsx-a11y"
+export * from "./react"
--- src/factory.js
+++ src/factory.js
@@ -4,20 +4,23 @@
  * @property {import("eslint").Linter.RulesRecord | undefined} [overrides] Rules overrides
  */
 
-import { isPackageExists } from "./utils"
-import { javascript, typescript } from "./configs"
+import { javascript, typescript, jsxA11y, react } from "./configs"
 
 /**
  * @param { Object } options
  * @param { FactoryPlugin } options.javascript JavaScript plugin configuration
  * @param { FactoryPlugin } options.typescript TypeScript plugin configuration
+ * @param { FactoryPlugin } options.jsxA11y `jsx-a11y` plugin configuration
+ * @param { FactoryPlugin } options.react React plugin configuration
  * @returns { import("eslint").Linter.FlatConfig[] }
  */
 export default function factory(options = {}) {
-  console.log("hellloooo")
+  console.log("Loading factory.js...")
   const {
     javascript: javascriptConfig = { enable: true, overrides: undefined },
-    typescript: typescriptConfig = { enable: true, overrides: undefined }
+    typescript: typescriptConfig = { enable: true, overrides: undefined },
+    jsxA11y: jsxA11yConfig = { enable: true, overrides: undefined },
+    react: reactConfig = { enable: true, overrides: undefined },
   } = options
 
   /** @type { import("eslint").Linter.FlatConfig[] } */
@@ -27,6 +30,14 @@ export default function factory(options = {}) {
 
   if (typescript.enable) {
     configs.push(typescript({ overrides: typescriptConfig.overrides }))
+  }
+
+  if (jsxA11yConfig.enable) {
+    configs.push(jsxA11y({ overrides: jsxA11yConfig.overrides }))
+  }
+
+  if (reactConfig.enable) {
+    configs.push(react({ overrides: reactConfig.overrides }))
   }
 
   return configs
